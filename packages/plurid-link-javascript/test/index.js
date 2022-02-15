596cfeb88c6bcb6cd0a73e8acf3364977264e34c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-02-15 17:49:42 +0200

    plurid-link-javascript > test: files

---
+++ packages/plurid-link-javascript/test/index.js
@@ -0,0 +1,23 @@
+const PluridLink = require('../distribution').default;
+
+
+
+const token = '__TEST__';
+
+const main = async () => {
+    const pluridLink = new PluridLink(token);
+
+    const response = await pluridLink.new({
+        type: 'service',
+        service: 'skype',
+        action: 'chat',
+    });
+    if (!response.status) {
+        return;
+    }
+    const link = response.data;
+
+    await pluridLink.remove(link);
+}
+
+main();
