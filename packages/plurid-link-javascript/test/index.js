59f183dfe6c9a719a68ec0472239050ee014224b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-02-15 20:35:42 +0200

    plurid-link-javascript > test: files

--- packages/plurid-link-javascript/test/index.js
+++ packages/plurid-link-javascript/test/index.js
@@ -9,10 +9,22 @@ const token = '__TEST__';
 const main = async () => {
     const pluridLink = new PluridLink(token);
 
+    // const response = await pluridLink.new({
+    //     type: 'service',
+    //     service: 'skype',
+    //     action: 'user',
+    //     data: {
+    //         chat: '',
+    //     },
+    // });
     const response = await pluridLink.new({
         type: 'service',
-        service: 'skype',
-        action: 'chat',
+        service: 'whatsapp',
+        action: 'send',
+        data: {
+            text: 'Hello!',
+            phone: '+1***********',
+        },
     });
     if (!response.status) {
         return;
